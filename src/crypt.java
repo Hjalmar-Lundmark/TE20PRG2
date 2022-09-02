dd772978307f1a4f1bbad36da8c683ba1313ed64
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-09-02 13:35:19 +0200

    Del 2 av crypterings uppg klar och på del tre kan man göra till hex men inte tillbaka än

--- src/crypt.java
+++ src/crypt.java
@@ -2,13 +2,71 @@ public class crypt {
     public static void main(String[] args) {
         char m = 'H';
         char k = '(';
+        String test = "Hej";
+        String crypted = cryptThisString(test, k);
+        String decrypted = deCryptThisString(crypted, k);
+
+        String hexCrypt = cryptHexThis(test, k);
+        //String hexDeCrypt = deCryptHexThis(hexCrypt, k);
+
         char c = (char) cryptThis(m, k);
         char v = (char) deCryptThis(c, k);
 
-        System.out.println(c);
+        System.out.println(hexCrypt);               // Del 3: Fungerar? tror jag
+        //System.out.println(hexDeCrypt);             // Del 3: Fungerar inte
+        //System.out.println(crypted);              // Del 2
+        //System.out.println(decrypted);            // Del 2
+        //System.out.println(c);                    // Del 1
+        //System.out.println(v);                    // Del 1
         //System.out.println(Integer.toBinaryString(c));
-        System.out.println(v);
         //System.out.println(Integer.toBinaryString(v));
+    }
+    /*
+    private static String deCryptHexThis(String in, char key) {
+        String hex = "";
+        String out = "";
+        for (int i = 0; i < in.length(); i++) {
+            char cIn = in.charAt(i);
+            char cIn2 = in.charAt(i+1);
+            hex = cIn + "" + cIn2;
+            String cOut = (String)(hex^key);
+            String sOut = Integer.toHexString(cOut);
+            out = out + sOut;
+        }
+        return out;
+    }
+
+     */
+
+    private static String cryptHexThis(String in, int key) {
+        String out = "";
+        for (int i = 0; i < in.length(); i++) {
+            char cIn = in.charAt(i);
+            char cOut = (char) (cIn^key);
+            String sOut = Integer.toHexString(cOut);
+            out = out + sOut;
+        }
+        return out;
+    }
+
+    private static String deCryptThisString(String in, int key) {
+        String out = "";
+        for (int i = 0; i < in.length(); i++) {
+            char cIn = in.charAt(i);
+            char cOut = (char) (cIn^key);
+            out = out + cOut;
+        }
+        return out;
+    }
+
+    private static String cryptThisString(String in, int key) {
+        String out = "";
+        for (int i = 0; i < in.length(); i++) {
+            char cIn = in.charAt(i);
+            char cOut = (char) (cIn^key);
+            out = out + cOut;
+        }
+        return out;
     }
 
     private static int deCryptThis(int c, int k) {
