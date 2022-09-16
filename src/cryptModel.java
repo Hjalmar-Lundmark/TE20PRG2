54b3f65df96e59b97be19a99e58f46ffc753ea48
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-09-16 13:37:45 +0200

    cryptModel fungerar

--- src/cryptModel.java
+++ src/cryptModel.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class cryptModel {
     String filnamn = "meddelande.txt";
     String meddelande;
     String keyfilnamn = "key.txt";
     String key;
-    private static String crypted;
+    String crypted;
 
     public void readTextFile() {
         FileReader fr = null;
@@ -28,18 +29,18 @@ public class cryptModel {
         crypted = encrypt(meddelande, key);
     }
 
-    private void readKey() {
-        FileReader fr = null;
+    public void readKey() {
+        FileReader fr2 = null;
         try {
-            fr = new FileReader(filnamn);
-            BufferedReader inFil = new BufferedReader(fr);
+            fr2 = new FileReader(keyfilnamn);
+            BufferedReader inFil = new BufferedReader(fr2);
             key = inFil.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private String encrypt(String meddelande, String key) {
+    public String encrypt(String meddelande, String key) {
         String out = "";
         char keey = key.charAt(0);
         for (int i = 0; i < meddelande.length(); i++) {
@@ -50,4 +51,76 @@ public class cryptModel {
         return out;
     }
 
+    public static void main(String[] args) {
+        cryptModel test = new cryptModel();
+
+        test.readTextFile();
+        test.crypt();
+
+        System.out.println(test.crypted);
+    }
 }
+// I psvm här över blir det problem om antingen inget annat är static
+// ELLER om jag inte gjort en variabel av programmet och kört i det(alltså vad som står här just över)
+// dock så testade jag att göra allt i static och det är det som är här under.
+// Både fungerar helt och likadant just nu.
+
+/*
+public class cryptModel {
+    static String filnamn = "meddelande.txt";
+    static String meddelande;
+    static String keyfilnamn = "key.txt";
+    static String key;
+    static String crypted;
+
+    public static String readTextFile(String filnamn, String meddelande) {
+        FileReader fr = null;
+        try {
+            fr = new FileReader(filnamn);
+            BufferedReader inFil = new BufferedReader(fr);
+            meddelande = inFil.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return meddelande;
+    }
+
+    public static String crypt(String key, String crypted, String meddelande) {
+        if (key == null) {
+            key = readKey(key, keyfilnamn);
+        }
+        crypted = encrypt(meddelande, key);
+        return crypted;
+    }
+
+    public static String  readKey(String key, String keyfilnamn) {
+        FileReader fr2 = null;
+        try {
+            fr2 = new FileReader(keyfilnamn);
+            BufferedReader inFil = new BufferedReader(fr2);
+            key = inFil.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return key;
+    }
+
+    public static String encrypt(String meddelande, String key) {
+        String out = "";
+        char keey = key.charAt(0);
+        for (int i = 0; i < meddelande.length(); i++) {
+            char cIn = meddelande.charAt(i);
+            char cOut = (char) (cIn^keey);
+            out = out + cOut;
+        }
+        return out;
+    }
+
+    public static void main(String[] args) {
+        meddelande = readTextFile(filnamn, meddelande);
+        crypted = crypt(key, crypted, meddelande);
+
+        System.out.println(crypted);
+    }
+}
+*/
