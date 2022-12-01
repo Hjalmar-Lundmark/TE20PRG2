c361a1a58c0ad53dbee7e2415bad63edae21d18e
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-12-01 13:48:13 +0100

    AOC day1, part1 complete

---
+++ src/AOC2022/dec1.java
@@ -0,0 +1,84 @@
+package AOC2022;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+public class dec1 {
+    public static void main(String[] args) {
+        int[] elf = new int[300];
+        String in = "";
+        String sting = "";
+        int p = 0;
+        int i = 0;
+
+        in = readTextFile2("dec1.txt");
+        while (true) {
+            while (true) {
+                sting = "";
+                if (p >= (in.length()-1)) {
+                    break;
+                }
+                for (int q = 0; q < 6; q++) {       //Creates a full number, ie 1234
+                    if (in.charAt(p) != ' ') {
+                        sting += in.charAt(p);
+                    }
+                    p++;
+                    if (in.charAt(p) == ' ') {
+                        break;
+                    }
+                }
+
+                if (sting != "") {          //calculates
+                    elf[i] += Integer.parseInt(sting);
+                } else {
+                    break;
+                }
+            }
+
+            i++;
+            //System.out.println(p);
+            //System.out.println(i);
+            if (p >= (in.length()-1)) {
+                break;
+            }
+        }
+
+        System.out.println("Klar");
+        //find biggest in elf
+        OptionalInt out = Arrays.stream(elf).max();
+        System.out.println(out);
+
+        //Part 2
+        IntStream qwerty = Arrays.stream(elf).sorted();
+        
+        System.out.println();
+    }
+
+    public static String readTextFile2(String filnamn) {
+        FileReader fr = null;
+        String meddelande = "";
+        String m2;
+        try {
+            fr = new FileReader(filnamn);
+            BufferedReader inFil = new BufferedReader(fr);
+            //meddelande += inFil.readLine();      //vilken linje läser den? Bara första?
+            m2 = inFil.readLine();
+            while (m2 != null) {
+                //System.out.println(meddelande);
+                // read next line
+                meddelande += m2;
+                meddelande += " ";
+                m2 = inFil.readLine();
+            }
+
+            inFil.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return meddelande;
+    }
+}
