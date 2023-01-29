6dff87560ae2e8b9fafadda0c291d762f9dad79f
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2023-01-29 01:45:42 +0100

    Forgot to commit on friday 💀

--- src/RandomBS/getLink.java
+++ src/RandomBS/getLink.java
@@ -8,16 +8,37 @@ public class getLink {
     public static void main(String[] args) throws IOException {
         URL resource = new URL("http://localhost:3000/"); // https doesn't works, http works
         Scanner scan = new Scanner(resource.openStream());
-        String s = "";
+        /*
+        HttpsURLConnection connection = (HttpsURLConnection)resource.openConnection();;
+        Scanner scan = new Scanner(connection.getInputStream());
+        */
+        String s;
         String[] a;
         while (scan.hasNext()) {
             s = scan.nextLine();
             //System.out.println(s);
             if (s.contains("<a href")) {
-                System.out.println(s);
+                //System.out.println(s);
                 a = s.split("\"");
-                System.out.println(a[1]);   //maybe not always work?
+                System.out.println(a[1]);
             }
         }
+
+        System.out.println("\nAPI: ");
+        System.out.println("Temperature is: " + getTemp("bygdea"));
+    }
+
+    public static String getTemp(String place) {
+        String request = "http://api.temperatur.nu/tnu_1.17.php?p=" + place + "&cli=api_demo";
+        String temperature = "";
+        try {
+            URL resource = new URL(request);
+            Scanner urlScanner = new Scanner(resource.openStream());
+            String result = urlScanner.nextLine();
+            temperature = result.split("\"")[23];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return temperature;
     }
 }
